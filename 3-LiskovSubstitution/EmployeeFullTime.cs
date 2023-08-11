ac30eb209f09f26a507f99a1fa529cdd7a5c0316
Jaime Alonso <jaimea111@gmail.com>
2023-08-11 02:05:50 -0500

    Adding Liskov substitution exercise

--- 3-LiskovSubstitution/EmployeeFullTime.cs
+++ 3-LiskovSubstitution/EmployeeFullTime.cs
@@ -2,8 +2,15 @@ namespace Liskov
 {
     public class EmployeeFullTime : Employee
     {
-        public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
+        public int _extraHours { get; set; }
+        public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked)
         {
+            _extraHours = extrahours;
         }
+        public override decimal CalculateSalary ()
+        {
+            decimal hourValue = 50;
+            return hourValue * (HoursWorked + _extraHours);
+        } 
     }
 }
--- 3-LiskovSubstitution/Program.cs
+++ 3-LiskovSubstitution/Program.cs
@@ -1,16 +1,14 @@
 ﻿using Liskov;
 
-CalculateSalaryMonthly(new List<Employee>() {
+ShowSalaryMonthly(new List<Employee>() {
     new EmployeeFullTime("Pepito Pérez", 160, 10),
-    new EmployeeContractor("Manuel Lopera", 180, 0)
+    new EmployeeContractor("Manuel Lopera", 180)
 });
 
-void CalculateSalaryMonthly(List<Employee> employees) 
+void ShowSalaryMonthly(List<Employee> employees) 
 {
     foreach (var item in employees)
     {
-        decimal salary = item.CalculateSalary((item is EmployeeFullTime));
-        Console.WriteLine($"The {item.Fullname}'s salary is {salary}");
-        
+        Console.WriteLine($"The {item.Fullname}'s salary is {item.CalculateSalary()}");
     }
 }
